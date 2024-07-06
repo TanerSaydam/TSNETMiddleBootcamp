8018ffd1982447a2206a7cd30c6b51e9fdcf12a1
TanerSaydam <tanersaydam@gmail.com>
2024-07-06 12:15:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TS NET Middle Eğitimi
+
+## 07.07.2024 (Sonraki ders)
+- Clean Architecture ile Todo app başladık devam edeceğiz
+- OData
+- Options Pattern
+- Generic Repository Pattern
+- Auto Mapper
+- Domain Event
+
+## 06.07.2024
+- Architectural Patterns
+- Diğer Mimari Türleri
+- Clean Architecture Giriş
+- Clean Architecture ile Todo app başladık
---
+++ TodoCleanArchitecture/TodoCleanArchitecture.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoCleanArchitecture.Domain.Entities;
+
+namespace TodoCleanArchitecture.Infrastructure.Context;
+internal sealed class ApplicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=TANER\\SQLEXPRESS;Initial Catalog=TodoCleanDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+
+    public DbSet<Todo> Todos { get; set; }
+}
